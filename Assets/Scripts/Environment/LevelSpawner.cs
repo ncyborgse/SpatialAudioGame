97444783faff047ac543a31ebd69daf325e85a03
Nils Borg <nils.ml.borg@gmail.com>
2025-02-20 14:59:04 +0100

    maps and start navigation

--- Assets/Scripts/Environment/LevelSpawner.cs
+++ Assets/Scripts/Environment/LevelSpawner.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using System.Collections;
 
-public class WallCollision : MonoBehaviour
+public class LevelSpawner : MonoBehaviour
 {
-    public AudioSource audioSource;
+
+    public string levelName;
+    public AudioSource loadLevelAudio;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,20 +24,16 @@ public class WallCollision : MonoBehaviour
         {
             PlayerController playerController = other.GetComponent<PlayerController>();
 
-            // Ensure played does not keep key
-            if (playerController != null) {
-                playerController.SetHasKey(false);
-            }
-            if (audioSource != null)
+            if (loadLevelAudio != null)
             {
-                audioSource.Play();
+                loadLevelAudio.Play();
                 // Load the scene after the sound finishes
-                StartCoroutine(LoadSceneAfterSound("SelectionScene"));
+                StartCoroutine(LoadSceneAfterSound(levelName));
             }
             else
             {
                 // Return to start scene
-                UnityEngine.SceneManagement.SceneManager.LoadScene("SelectionScene");
+                UnityEngine.SceneManagement.SceneManager.LoadScene(levelName);
             }
         }
     }
@@ -49,3 +47,4 @@ public class WallCollision : MonoBehaviour
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
     }
 }
+
